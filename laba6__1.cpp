f698ddb571b95de8e5d8aa881e18c56a939f1996
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-11-02 19:55:20 +0200

    1

---
+++ laba6__1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+void create(int* a, const int p)
+{
+    for (int i = 0; i <= p; i++)
+    {
+        a[i] = -40 + rand() % (91);
+        if ((a[i] > 0 && (a[i] % 5) != 0) || (a[i] < 0))
+            cout << a[i] << ", ";
+    }
+}
+
+void zavd(int* a, int p, int& S, int& n)
+{
+    for (int i = 0; i <= p; i++)
+    {
+        S += a[i];
+        a[i] = 0;
+        n += 1;
+    }
+}
+
+
+void end(int* a, int S, int number, int p)
+{
+    cout << endl << "Sum = " << S << endl;
+    cout << "number = " << number << endl;
+
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    const int p = 21;
+    int b[p + 1];
+    int S = 0;
+    int number = 0;
+
+
+    create(b, p);
+    zavd(b, p, S, number);
+    end(b, S, number, p);
+
+    return 0;
+}
